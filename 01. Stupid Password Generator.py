8cd73ba110191ef1b29516ae86c1d147973ebc58
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-10 21:28:49 +0200

    01.thirth commit

--- 01. Stupid Password Generator.py
+++ 01. Stupid Password Generator.py
@@ -3,3 +3,8 @@ l = int(input())
 
 result = ''
 
+for s1 in range(1, n + 1):
+for s2 in range(1, n + 1):
+for s3 in range(97, l + 97):
+for s4 in range(97, l + 97):
+for s5 in range(max(s1,s2) + 1, n + 1):
