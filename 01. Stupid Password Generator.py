2e18ad7fc09d08987edf9f5d1167d215d67c6c20
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-10 21:31:40 +0200

    all most finish the first task

--- 01. Stupid Password Generator.py
+++ 01. Stupid Password Generator.py
@@ -4,7 +4,10 @@ l = int(input())
 result = ''
 
 for s1 in range(1, n + 1):
-for s2 in range(1, n + 1):
-for s3 in range(97, l + 97):
-for s4 in range(97, l + 97):
-for s5 in range(max(s1,s2) + 1, n + 1):
+    for s2 in range(1, n + 1):
+        for s3 in range(97, l + 97):
+            for s4 in range(97, l + 97):
+                for s5 in range(max(s1,s2) + 1, n + 1):
+                    result += ('' + str(s1) + str(s2) 
+                                  + str(s3) + str(s4) 
+                                  + str(s5 + ''))
