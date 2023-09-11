981fcf980b71aee8ce16488dd7349352a7b526e8
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-11 22:26:02 +0200

    first two task are finish

--- 02. Magic Numbers.py
+++ 02. Magic Numbers.py
@@ -12,31 +12,32 @@ for a in range(1, 10):
                                           + str(e) + str(f) + '')
 print(result)
 
-# Second way
-n = int(input())
-result = ''
-a = 1
+## Second way
 
-while a <= 9:
-    b = 1
-    while b <= 9:
-        c = 1
-        while c <= 9:
-            d = 1
-            while d <= 9:
-                e = 1
-                while e <= 9:
-                    f = 1
-                    while f <= 9:
-                        if a * b * c *d * e * f == n:
-                            if a * b * c * d * e * f == n:
-                            result += ('' + str(a) + str(b) 
-                                          + str(c) + str(d) 
-                                          + str(e) + str(f) + ' ')
-                        f += 1
-                    e += 1
-                d += 1
-            c += 1
-        b += 1
-    a += 1
-print(result)                            
+# n = int(input())
+# result = ''
+# a = 1
+
+# while a <= 9:
+#     b = 1
+#     while b <= 9:
+#         c = 1
+#         while c <= 9:
+#             d = 1
+#             while d <= 9:
+#                 e = 1
+#                 while e <= 9:
+#                     f = 1
+#                     while f <= 9:
+#                         if a * b * c *d * e * f == n:
+#                             if a * b * c * d * e * f == n:
+#                             result += ('' + str(a) + str(b) 
+#                                           + str(c) + str(d) 
+#                                           + str(e) + str(f) + ' ')
+#                         f += 1
+#                     e += 1
+#                 d += 1
+#             c += 1
+#         b += 1
+#     a += 1
+# print(result)                            
