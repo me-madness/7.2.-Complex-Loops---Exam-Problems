5eb8b108589e08ef3a13344f8084b1bfaace03b3
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-11 22:20:25 +0200

    02. second way

--- 02. Magic Numbers.py
+++ 02. Magic Numbers.py
@@ -11,3 +11,17 @@ for a in range(1, 10):
                                           + str(c) + str(d) 
                                           + str(e) + str(f) + '')
 print(result)
+
+# Second way
+n = int(input())
+result = ''
+a = 1
+
+while a <= 9:
+    b = 1
+    while b <= 9:
+        c = 1
+        while c <= 9:
+            d = 1
+            while d <= 9:
+                
