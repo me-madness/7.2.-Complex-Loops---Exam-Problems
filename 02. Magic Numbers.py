5f99e300733af739d99be94718a6d52fcfa65cb6
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-11 22:08:52 +0200

    02.if check

--- 02. Magic Numbers.py
+++ 02. Magic Numbers.py
@@ -6,4 +6,5 @@ for a in range(1, 10):
             for d in range(1, 10):
                 for e in range(1, 10):
                     for f in range(1, 10):
-                        if a * b
+                        if a * b * c * d * e * f == n:
+                            
